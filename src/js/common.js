7f9d88d1356b81bda42b79c9fd32fd53930d0e9a
maxim <maximum454@gmail.com>
2019-02-13 12:30:09 +0600

    webpack

---
+++ src/js/common.js
@@ -0,0 +1,5 @@
+/**
+ * Created by maxim on 2/5/19.
+ */
+let add = (a,b) => a+b;
+console.log(add(2,5));
--- webpack.config.js
+++ webpack.config.js
@@ -2,49 +2,113 @@
  * Created by maxim on 2/2/19.
  */
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 module.exports = {
     mode: 'development',
     entry: {
-        app: './src/index.js'
+        moneyup: './src/index.js'
+    },
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: './'
     },
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist',
-        hot: true
+        overlay: true
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title: 'Output Management'
+            template: './src/index.html'
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+        })
     ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    },
+
     module: {
         rules: [
             {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
                 use: [
-                    'file-loader'
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            config: {
+                                path: 'postcss.config.js'
+                            }
+                        }
+                    },
                 ]
             },
             {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                test: /\.scss$/,
                 use: [
-                    'file-loader'
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    }, {
+                        loader: 'postcss-loader',
+                        options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }
+                    }, {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
                 ]
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png|svg)$/,
+                exclude: /fonts/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            useRelativePath: true
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 80,
+
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|svg)$/,
+                exclude: /node_modules/,
+                loader: 'url-loader?limit=1024&name=fonts/[name].[ext]'
             }
         ]
     }
